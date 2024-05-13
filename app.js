e537f14dcaae0a32b96e2f845eaf75a3ad53e9e4
Haiaza <ajames1342@gmail.com>
2024-05-12 21:28:49 -0400

    Im not sure I completed Q10 correctly

--- app.js
+++ app.js
@@ -114,3 +114,23 @@ Complete Exercise 9 in the space below:
 let allFoods = foods.join(' -> ')
 
 console.log('Exercise 9 result:', allFoods);
+/*
+Exercise 10: Check for an element
+
+1) Using the .includes() method, check if the `foods` array contains the string
+   'soup'.
+
+2) Assign the result to a variable called `hasSoup``.
+
+Complete Exercise 10 in the space below:
+*/
+
+function hasSoup(){
+    if (foods.includes('soup')){
+        return true
+    } else {
+        return
+    }
+}
+
+console.log('Exercise 10 result:', hasSoup);
