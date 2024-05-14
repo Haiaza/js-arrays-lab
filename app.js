0dffacda2c90d6728de6774ec6002d1e2c3304ad
haiaza <83097184+Haiaza@users.noreply.github.com>
2024-05-14 21:17:58 +0000

    Last question! Im confused here

--- app.js
+++ app.js
@@ -227,3 +227,30 @@ Complete Exercise 14 in the space below:
 num = numArrays[2][1];
 
 console.log("Exercise 14 result:", num);
+/*
+Exercise 15: Nested array sum
+
+1) Use nested loops or `forEach()` methods to sum up all numbers within 
+   `numArrays` nested arrays.
+   
+2) Assign the sum to a variable called `total`.
+
+Hint: Be sure to declare and initialize the total variable before the iterations.
+
+Complete Exercise 15 in the space below:
+*/
+
+/**const numArrays = [
+  [100, 5, 23],
+  [15, 21, 72, 9],
+  [45, 66],
+  [7, 81, 90],
+]; */
+
+const total = 0;
+
+total.forEach(arrayRow){
+  
+}
+
+console.log("Exercise 15 result:\n", total);
